daa75b90bfe5cc84fde88e56b39e66af4c314e77
jkomijani <jkomijani@gmail.com>
2023-09-30 14:36:39 +0200

    _hack in MatrixModule_ modified.

--- src/nn/matrix/matrix_module_.py
+++ src/nn/matrix/matrix_module_.py
@@ -31,7 +31,7 @@ class MatrixModule_(Module_):
     def backward(self, x, log0=0, reduce_=False):
         return self._kernel(x, log0=log0, reduce_=reduce_, forward=False)
 
-    def _kernel(self, matrix, log0=0, forward=True, reduce_=False):
+    def _kernel(self, matrix, *, forward, reduce_, log0=0):
         """Return the transformed matrix and its Jacobian.
 
         To this end, `matrix_handle` is used for parametrizing the input
@@ -63,19 +63,37 @@ class MatrixModule_(Module_):
 
         return matrix, log0 + logJ
 
-    def _hack(self, matrix, log0=0, reduce_=False):
-        """Similar to the forward method, but returns intermediate parts too."""
+    def _hack(self, matrix, log0=0, forward=True, reduce_=False):
+        """Similar to the forward/backward methods, but returns intermediate
+        parts too.
+        """
+        # 1. Parametrize the input matrix
         param, logJ_mat2par = self.matrix_handle.matrix2param_(matrix)
         stack = [(param, logJ_mat2par)]
 
+        # 2. Move the channel axis, in which the param are listed, from -1 to 1
         param = torch.movedim(param, -1, 1)  # move channel axis from -1 to 1
-        param, logJ_par2par = self.net_(param)
+
+        # 3. Transform param
+        if forward:
+            param, logJ_par2par = self.net_.forward(param)
+        else:
+            param, logJ_par2par = self.net_.backward(param)
+
+        # 4. Move back the channel axis to -1
         param = torch.movedim(param, 1, -1)  # return channel axis to -1
         stack.append((param, logJ_par2par))
 
+        # 5. Construct a new matrix from the transformed parameters
+        matrix, logJ_par2mat = \
         matrix, logJ_par2mat = \
                 self.matrix_handle.param2matrix_(param, reduce_=reduce_)
         stack.append((matrix, logJ_par2mat))
+
+        # 6. Add up all log-Jacobians
+        logJ = logJ_mat2par + logJ_par2par + logJ_par2mat
+        stack.append((matrix, log0 + logJ))
+
         return stack
 
     def transfer(self, **kwargs):
