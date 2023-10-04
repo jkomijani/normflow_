3e8bd68b65937073476a3197ddb4593bc89ba776
jkomijani <jkomijani@gmail.com>
2023-10-04 11:29:02 +0200

    GaugeLinksDoubleMask is added to &  MatrixMask is removed from __init__

--- src/mask/__init__.py
+++ src/mask/__init__.py
@@ -1,5 +1,8 @@
 from .mask import EvenOddMask
 from .mask import AlongAxesEvenOddMask
-from .mask import ListSplitMask, DoubleMask
-from .matrix_mask import MatrixMask
+from .mask import ListSplitMask
+
+from .double_mask import DoubleMask
+from .double_mask import GaugeLinksDoubleMask
+
 from .planar_mask import ZebraPlanarMask
