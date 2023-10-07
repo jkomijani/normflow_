2369af68b38dd192053e066663fcc80cfcadfd65
jkomijani <jkomijani@gmail.com>
2023-10-07 19:37:32 +0200

    GaugeLinksDoubleMask is remove now from __init__

--- src/mask/__init__.py
+++ src/mask/__init__.py
@@ -3,6 +3,5 @@ from .mask import AlongAxesEvenOddMask
 from .mask import ListSplitMask
 
 from .double_mask import DoubleMask
-from .double_mask import GaugeLinksDoubleMask
 
 from .planar_mask import ZebraPlanarMask
