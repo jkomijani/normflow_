e525cf1e5988e734c7e600230e1baefb47b0f51b
Javad Komijani <jkomijani@phys.ethz.ch>
2023-10-16 19:26:05 +0200

    SchwingerAction is commented out for now

--- src/action/__init__.py
+++ src/action/__init__.py
@@ -1,5 +1,5 @@
-from .scalar_action import ScalarPhi4Action
-from .matrix_action import MatrixAction
 from .gauge_action import GaugeAction, U1GaugeAction
-from .schwinger_action import SchwingerAction
 from .ginibre_gauge_action import GinibreGaugeAction
+from .matrix_action import MatrixAction
+from .scalar_action import ScalarPhi4Action
+# from .schwinger_action import SchwingerAction
