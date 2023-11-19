e4f9dfedca3544eb7d754fe5ae724242a66e849f
jkomijani <jkomijani@gmail.com>
2023-11-19 22:34:48 +0100

    Partitioners are updated.

--- src/mask/partitioner.py
+++ src/mask/partitioner.py
@@ -2,6 +2,8 @@
 
 """Similar to mask, except for the shape of the input and output tensors."""
 
+import torch
+
 
 class ListPartitioner:
 
@@ -19,7 +21,7 @@ class ListPartitioner:
 
 
 class ChunkCatPartitioner:
-    """For chunking the input along the `chunk_axis`."""
+    """For chunking the input along the chunk_axis, which must be positive."""
 
     def __init__(self, chunk_axis):
         self.axis = chunk_axis
@@ -36,20 +38,26 @@ class ChunkCatPartitioner:
 
 
 class AlongAxisEvenOddPartitioner:
-    """For even odd slicing the input along the `even_odd_axis`."""
+    """For even odd slicing the input along the even_odd_axis, which must be
+    positive.
+    """
 
     def __init__(self, even_odd_axis):
         self.axis = even_odd_axis
-        self.even_ind = [slice(None)] * (self.axis - 1) + [slice(0, None, 2)]
-        self.odd_ind = [slice(None)] * (self.axis - 1) + [slice(1, None, 2)]
+        self.even_ind = [slice(None)] * self.axis + [slice(0, None, 2)]
+        self.odd_ind = [slice(None)] * self.axis + [slice(1, None, 2)]
 
     def split(self, x):
         return x[self.even_ind], x[self.odd_ind]
 
     def cat(self, x_even, x_odd):
         shape = list(x_even.shape) 
-        shape[self.axis] *= 2
+        shape[self.axis] = x_even.shape[self.axis] + x_odd.shape[self.axis]
         x = torch.zeros(shape, dtype=x_even.dtype, device=x_even.device)
         x[self.even_ind] = x_even
         x[self.odd_ind] = x_odd
         return x
+
+    @staticmethod
+    def purify(x_chnl, *args, **kwargs):
+        return x_chnl
